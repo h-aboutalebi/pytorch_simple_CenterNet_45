b74a26e8d50097e698d539a378a7f2c47ca77e70
hossein <hossain.aboutalebi@gmail.com>
2021-07-30 12:20:17 -0400

    Added parser

--- coco_convertor.py
+++ coco_convertor.py
@@ -1,5 +1,6 @@
 import glob
 import fiftyone as fo
+import os
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
@@ -7,6 +8,7 @@ from sklearn import preprocessing
 from parse_csv import Parser
 
 csv_output = "/home/hossein/Desktop/dataset/RSNA_COMPETE/hossein_train.csv"
+export_output = "/home/hossein/Desktop/dataset/RSNA_COMPETE/coco"
 images_patt = "/home/hossein/Desktop/dataset/RSNA_COMPETE/train_png"
 train_image_level_path = "/home/hossein/Desktop/dataset/RSNA_COMPETE/train_image_level.csv"  # contains bboxes
 train_study_level_path = "/home/hossein/Desktop/dataset/RSNA_COMPETE/train_study_level.csv"
@@ -22,37 +24,16 @@ eval_percent = 0.1
 # }
 
 parser = Parser(img_pth=images_patt, load=True, load_path=csv_output)
-train_image_df, eval_image_df = parser.parse_RSNA_csv(train_image_level_path=train_image_level_path,
+train_image_df= parser.parse_RSNA_csv(train_image_level_path=train_image_level_path,
                                                       train_study_level_path=train_study_level_path,
-                                                      eval_percent=eval_percent,
-                                                      csv_output=csv_output)
+                                                      eval_percent=eval_percent)
 
-# Create dataset
-dataset = fo.Dataset(name="RSNA_train_dataset")
+eval_dataset=parser.coco_covert(train_image_df,name="RSNA_train_dataset")
+label_field = "ground_truth"  # for example
 
-# Persist the dataset on disk in order to
-# be able to load it in one line in the future
-dataset.persistent = True
-
-# Add your samples to the dataset
-for filepath in glob.glob(images_patt):
-    sample = fo.Sample(filepath=filepath)
-
-    # Convert detections to FiftyOne format
-    detections = []
-    for obj in annotations[filepath]:
-        label = obj["label"]
-
-        # Bounding box coordinates should be relative values
-        # in [0, 1] in the following format:
-        # [top-left-x, top-left-y, width, height]
-        bounding_box = obj["bbox"]
-
-        detections.append(
-            fo.Detection(label=label, bounding_box=bounding_box)
-        )
-
-    # Store detections in a field name of your choice
-    sample["ground_truth"] = fo.Detections(detections=detections)
-
-    dataset.add_sample(sample)
+# Export the dataset
+eval_dataset.export(
+    export_dir=export_output,
+    dataset_type=fo.types.COCODetectionDataset,
+    label_field=label_field,
+)
--- parse_csv.py
+++ parse_csv.py
@@ -5,6 +5,7 @@ import glob
 import os
 import fiftyone as fo
 from sklearn import preprocessing
+from alive_progress import alive_bar
 
 
 class Parser:
@@ -12,7 +13,7 @@ class Parser:
     def __init__(self, img_pth, load, load_path=None):
         self.img_pth = img_pth
         self.load = load
-        self.load_path=load_path
+        self.load_path = load_path
 
     def add_remainder(self, df):
         added_rows = []
@@ -38,9 +39,8 @@ class Parser:
                 width, height = Image.open(os.path.join(root, file)).size
                 image_name = str(file.split("_")[1][:-4]) + "_image"
                 df.loc[df['id'] == image_name, ["width", "height"]] = [width, height]
-            return df
 
-    def parse_RSNA_csv(self, train_image_level_path, train_study_level_path, eval_percent, csv_output):
+    def parse_RSNA_csv(self, train_image_level_path, train_study_level_path, eval_percent):
         if (self.load is False):
             train_study_df = pd.read_csv(train_study_level_path)
             train_study_df = train_study_df.rename(
@@ -76,48 +76,70 @@ class Parser:
             print("list classes integer_labels: {}".format(le.classes_))
             train_image_df = train_image_df.sample(frac=1).reset_index(drop=True)
             print(train_image_df.head(10).T)
-            number_eval = int(len(train_image_df.index) * eval_percent)
 
             # saving dataframe:
-            train_image_df.to_csv(csv_output)
+            train_image_df.to_csv(self.load_path)
             print("saved csv file")
         else:
             train_image_df = pd.read_csv(self.load_path)
 
-        eval_image_df = train_image_df.iloc[:number_eval]
-        train_image_df = train_image_df.iloc[number_eval:]
-        print("len trainset: {}".format(train_image_df.shape[0]))
-        print("len evalset: {}".format(eval_image_df.shape[0]))
+        number_eval = int(len(train_image_df.index) * eval_percent)
+        # eval_image_df = train_image_df.iloc[:number_eval]
+        # train_image_df = train_image_df.iloc[number_eval:]
+        # print("len trainset: {}".format(train_image_df.shape[0]))
+        # print("len evalset: {}".format(eval_image_df.shape[0]))
         train_image_df = self.add_remainder(train_image_df)
-        eval_image_df = self.add_remainder(eval_image_df)
-        return train_image_df, eval_image_df
+        # eval_image_df = self.add_remainder(eval_image_df)
+        return train_image_df #, eval_image_df
 
-    def coco_covert(self, images_patt, df, name="RSNA_train_dataset"):
-        dataset = fo.Dataset(name=name)
+    def coco_covert(self, df, name="RSNA_train_dataset"):
+        try:
+            dataset = fo.Dataset(name=name)
+        except Exception as e:
+            print("ERROR : " + str(e) + "\nfixing it with loading")
+            dataset = fo.load_dataset(name=name)
+            dataset.delete()
+            return self.coco_covert(df,name=name)
 
         # Persist the dataset on disk in order to
         # be able to load it in one line in the future
         dataset.persistent = True
 
         # Add your samples to the dataset
-        for filepath in glob.glob(images_patt):
-            sample = fo.Sample(filepath=filepath)
+        with alive_bar(1000) as bar:
+            for index, row in df.iterrows():
+                bar()
+                filepath = os.path.join(self.img_pth, row["StudyInstanceUID"] + "_" + row["id"][:-6] + ".png")
+                if (os.path.isfile(filepath) is False):
+                    continue
+                sample = fo.Sample(filepath=filepath)
 
-            # Convert detections to FiftyOne format
-            detections = []
-            for obj in df[filepath]:
-                label = obj["label"]
+                # Convert detections to FiftyOne format
+                detections = []
+                label = row["label_y"]
 
                 # Bounding box coordinates should be relative values
                 # in [0, 1] in the following format:
                 # [top-left-x, top-left-y, width, height]
-                bounding_box = obj["bbox"]
+                bounding_box = self.compute_bounding_box(row["x_min"], row["y_min"], row["x_max"], row["y_max"],
+                                                         row["width"], row["height"])
 
                 detections.append(
                     fo.Detection(label=label, bounding_box=bounding_box)
                 )
 
-            # Store detections in a field name of your choice
-            sample["ground_truth"] = fo.Detections(detections=detections)
+                # Store detections in a field name of your choice
+                sample["ground_truth"] = fo.Detections(detections=detections)
 
-            dataset.add_sample(sample)
+                dataset.add_sample(sample)
+        return dataset
+
+    def compute_bounding_box(self, x_min, y_min, x_max, y_max, img_width, img_height):
+        x_min,x_max=float(x_min),float(x_max)
+        y_min,y_max=float(y_min),float(y_max)
+        img_height,img_height=float(img_width),float(img_height)
+        top_left_x = x_min / img_width
+        top_left_y = y_max / img_height
+        width = (x_max - x_min) / img_width
+        height = (y_max - y_min) / img_height
+        return [top_left_x, top_left_y, width, height]
